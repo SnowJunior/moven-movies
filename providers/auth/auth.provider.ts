b0111e136651ba0a75a5c0fe18a3285bcd12e508
George Onyango <onyangogeorge954@gmail.com>
2025-01-24 20:54:11 +0300

    chore: setup firebase as auth provider

--- providers/auth/auth.provider.ts
+++ providers/auth/auth.provider.ts
@@ -1,15 +1,30 @@
 /* eslint-disable @typescript-eslint/no-explicit-any */
-import { getAuth, createUserWithEmailAndPassword, signInWithEmailAndPassword } from "firebase/auth";
+import {
+  getAuth,
+  createUserWithEmailAndPassword,
+  signInWithEmailAndPassword,
+  User,
+} from "firebase/auth";
 import { getFirestore, doc, setDoc } from "firebase/firestore";
+import { firebaseApp } from "../firebase/firebase.provider";
 
-const firestore = getFirestore();
+const firestore = getFirestore(firebaseApp);
 
-export async function registerUser(email: string, password: string, additionalData?: Record<string, any>) {
-  const auth = getAuth();
+export async function registerUser(
+  email: string,
+  password: string,
+  username: string,
+  additionalData?: Record<string, any>
+) {
+  const auth = getAuth(firebaseApp);
 
   try {
     // Create the user in Firebase Authentication
-    const userCredential = await createUserWithEmailAndPassword(auth, email, password);
+    const userCredential = await createUserWithEmailAndPassword(
+      auth,
+      email,
+      password
+    );
     const user = userCredential.user;
 
     // Save the user to Firestore
@@ -17,29 +32,44 @@ export async function registerUser(email: string, password: string, additionalDa
       uid: user.uid,
       email: user.email,
       createdAt: new Date().toISOString(),
+      username: username,
       ...additionalData,
     });
 
     console.log("User registered and saved to Firestore:", user);
     return user;
   } catch (error: any) {
-    console.error("Error registering user:", error.message);
-    throw new Error(error.message); // Re-throw the error to handle it in the UI
+    return {
+      message: error.message,
+    };
   }
 }
 
-export async function loginUser(email: string, password: string) {
-  const auth = getAuth();
+export async function loginUser(
+  email: string,
+  password: string
+): Promise<{ user?: User; message: string, success: boolean }> {
+  const auth = getAuth(firebaseApp);
 
   try {
     // Log in the user
-    const userCredential = await signInWithEmailAndPassword(auth, email, password);
+    const userCredential = await signInWithEmailAndPassword(
+      auth,
+      email,
+      password
+    );
     const user = userCredential.user;
 
     console.log("User logged in:", user);
-    return user;
+    return {
+      success: true,
+      user,
+      message: "Successful login",
+    };
   } catch (error: any) {
-    console.error("Error logging in:", error.message);
-    throw new Error(error.message); // Re-throw the error to handle it in the UI
+    return {
+      success: false,
+      message: error.message,
+    };
   }
 }
