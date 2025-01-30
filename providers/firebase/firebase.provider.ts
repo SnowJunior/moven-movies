b96b5d1ad4d067ff8a390c6d88bf8bd9393e58fb
George Onyango <onyangogeorge954@gmail.com>
2025-01-30 09:00:44 +0300

    fix: update firebase imports

--- providers/firebase/firebase.provider.ts
+++ providers/firebase/firebase.provider.ts
@@ -1,16 +1,16 @@
 // Import the functions you need from the SDKs you need
-import { firebaseAPI, firebaseAPPID, firebaseMessangerID } from "@/utils/utils";
+import { apiKey, appID, messangerID } from "@/utils/utils";
 import { initializeApp } from "firebase/app";
 // https://firebase.google.com/docs/web/setup#available-libraries
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: firebaseAPI,
+  apiKey: apiKey,
   authDomain: "moven-movies.firebaseapp.com",
   projectId: "moven-movies",
   storageBucket: "moven-movies.firebasestorage.app",
-  messagingSenderId: firebaseMessangerID,
-  appId: firebaseAPPID
+  messagingSenderId: messangerID,
+  appId: appID
 };
 
 // Initialize Firebase
