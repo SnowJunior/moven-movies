15492f86347f3f49bc6b8bb3e0451b62ec23ca01
George Onyango <onyangogeorge954@gmail.com>
2025-01-30 08:58:02 +0300

    build: update env variables naming

--- utils/utils.test.tsx
+++ utils/utils.test.tsx
@@ -4,9 +4,9 @@ import {
   apiClient,
   baseURL,
   imageURL,
-  firebaseAPI,
-  firebaseMessangerID,
-  firebaseAPPID,
+  apiKey,
+  messangerID,
+  appID,
 } from "./utils"; // Adjust path
 
 jest.mock("axios");
@@ -89,10 +89,10 @@ describe("apiClient", () => {
   it("should export environment variables", () => {
     expect(baseURL).toBe(process.env.NEXT_PUBLIC_API_URL);
     expect(imageURL).toBe("https://image.tmdb.org/t/p/original");
-    expect(firebaseAPI).toBe(process.env.NEXT_PUBLIC_FIREBASE_API);
-    expect(firebaseMessangerID).toBe(
-      process.env.NEXT_PUBLIC_FIREBASE_MESSENGER_ID
+    expect(apiKey).toBe(process.env.FIREBASE_API_KEY);
+    expect(messangerID).toBe(
+      process.env.FIREBASE_MESSENGER_ID
     );
-    expect(firebaseAPPID).toBe(process.env.NEXT_PUBLIC_FIREBASE_APP_ID);
+    expect(appID).toBe(process.env.FIREBASE_APP_ID);
   });
 });
